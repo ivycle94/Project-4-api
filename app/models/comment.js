09501f50737c8c85a73f932b987a825daa346579
Ivycle94 <Ivycle94@gmail.com>
2022-04-25 09:43:49 -0500

    added comment model

---
+++ app/models/comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+    note: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = commentSchema
